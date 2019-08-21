4e18f5113b8f4353723dba4b823db09b13d7ee43
Michael Arakilian <arakilian0@gmail.com>
2019-08-21 14:34:23 -0700

    UNINSTALLED progress and deleted test files

--- bin/cli.js
+++ bin/cli.js
@@ -5,36 +5,24 @@ let print = console.log,
     help = require('./../msg/msg');
 
 switch(argsArray[0]) {
-	// commands
 	case "i":
   case "init":
 		argsArray.shift();
     require('./../cmd/init/init')(argsArray);
     break;
-
   case "u":
   case "update":
 		argsArray.shift();
 		require('./../cmd/update/update')(argsArray);
     break;
-
-  // case "test":
-  // case "test":
-	// 	argsArray.shift();
-	// 	require('./../cmd/test/test')(argsArray);
-  //   break;
-
-	// options
   case "-h":
   case "--help":
 		help.main();
     break;
-
   case "-v":
   case "--version":
 		help.version();
     break;
-
   default:
     help.generic();
 }
